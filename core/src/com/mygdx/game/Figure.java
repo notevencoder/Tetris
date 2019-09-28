e98207fec5bd734ab72a78fba99e96d8997aa4bd
Akamoto529 <akamoto01031982@gmail.com>
2019-09-28 21:52:41 +0300

    Добавлена палка

--- core/src/com/mygdx/game/Figure.java
+++ core/src/com/mygdx/game/Figure.java
@@ -2,24 +2,21 @@ package com.mygdx.game;
 
 public abstract class Figure {
     boolean isLanded = false;
-    int color;
-    int x , y;
-    int[][] mas;
+    int color;          //Цвет фигуры
+    int x , y;          //Координаты центральной точки фигуры
+    int rot =0;            //Фаза вращения фигуры
+    int[][] mas;        //Массив поля
+    //Конструктор
     Figure(int color, int SpawnX, int SpawnY, int[][] mas) {
         this.color = color;
         this.mas = mas;
         x = SpawnX;
         y = SpawnY;
     }
-
-    void update(){
-        fall();
-    }
-
-
     abstract void spawn(int x, int y);
     abstract void move();
     abstract void fall();
+    abstract void rotate();
     abstract void draw();
     abstract void clear();
 
